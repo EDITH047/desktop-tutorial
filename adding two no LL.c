dbc0e72a8e5460c7e21fdcbd89d0bb02fd3073e2
EDITH047 <anshifirozpro@gmail.com>
2025-08-24 17:06:05 +0000

    Auto update - Sun Aug 24 17:06:05 UTC 2025

---
+++ adding two no LL.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node *head1 = NULL;
+struct Node *head2 = NULL;
+struct Node *result = NULL;
+
+void create1(int data)
+{
+    struct Node *temp, *newNode;
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode==NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    if (head1 == NULL)
+    {
+        head1 = newNode;
+    }
+    else 
+    {
+        temp = head1;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void create2(int data)
+{
+    struct Node *temp, *newNode;
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode==NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    if (head2 == NULL)
+    {
+        head2 = newNode;
+    }
+    else 
+    {
+        temp = head2;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void create3(int data)
+{
+    struct Node *temp, *newNode;
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode==NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    if (result == NULL)
+    {
+        result = newNode;
+    }
+    else 
+    {
+        temp = result;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void reverse(struct Node **head)
+{
+    struct Node *prev = NULL, *current = *head, *next = NULL;
+    while (current != NULL) 
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+int main() 
+{
+    int data;
+    printf("Enter the first number(Enter the digits from left to right and -1 to exit): ");
+    while (1) {
+        scanf("%d", &data);
+        if (data == -1) {
+            break;
+        }
+        create1(data);
+    }
+    printf("Enter the second number(Enter the digits from left to right and -1 to exit): ");
+    while (1) {
+        scanf("%d", &data);
+        if (data == -1) {
+            break;
+        }
+        create2(data);
+    }
+    reverse(&head1);
+    reverse(&head2);
+    struct Node *ptr1 = head1;
+    struct Node *ptr2 = head2;
+    int carry = 0;
+    while (ptr1 != NULL || ptr2 != NULL || carry != 0) {
+        int sum = carry;
+        if (ptr1 != NULL) {
+            sum += ptr1->data;
+            ptr1 = ptr1->next;
+        }
+        if (ptr2 != NULL) {
+            sum += ptr2->data;
+            ptr2 = ptr2->next;
+        }
+        carry = sum / 10;
+        create3(sum % 10);
+    }
+    reverse(&result);
+    printf("Sum: ");
+    struct Node *temp = result;
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+    return 0;
+}
