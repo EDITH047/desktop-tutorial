0c1940373541ae4268380e289210ea4be6d28f82
EDITH047 <anshifirozpro@gmail.com>
2025-09-15 17:24:51 +0000

    Auto update - Mon Sep 15 17:24:51 UTC 2025

--- postfix prefix binary tree.c
+++ postfix prefix binary tree.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 #define MAX 100
-char stack[MAX];
-int top = -1;
-int n;
-char arr[100];
-struct Node
-{
+
+// Expression Tree Node
+struct Node {
     char data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left, *right;
 };
-struct Node *root = NULL;
-struct Node* pop() {
-    if (top == -1)
-        return NULL;
-    else {
-        struct Node* temp = (struct Node*)stack[top];
-        top--;
-        return temp;
-    }
+
+// Stack for Nodes
+struct Node* stackNode[MAX];
+int topNode = -1;
+
+void pushNode(struct Node* n) {
+    stackNode[++topNode] = n;
 }
-void push(struct Node* temp) {
-    if (top == MAX - 1)
-        return;
-    else {
-        top++;
-        stack[top] = (char)temp;
-    }
+struct Node* popNode() {
+    return stackNode[topNode--];
 }
+
+// Stack for operators
+char stackOp[MAX];
+int topOp = -1;
+
+void pushOp(char c) {
+    stackOp[++topOp] = c;
+}
+char popOp() {
+    return stackOp[topOp--];
+}
+char peekOp() {
+    return stackOp[topOp];
+}
+int isEmptyOp() {
+    return topOp == -1;
+}
+
 int isp(char temp) {
     if ((temp == '-') || (temp == '+')) 
         return 2;
@@ -58,51 +67,96 @@ int icp(char temp) {
         return -1;
     }
 }
-char traverse()
-{
-    struct Node* node = root;
-    if (node == NULL) {
-        return '\0';
-    }
-    char left = traverse(node->left);
-    char right = traverse(node->right);
-    return node->data;
+
+// Create new Node
+struct Node* newNode(char c) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = c;
+    temp->left = temp->right = NULL;
+    return temp;
 }
-void postfix()
-{ 
-    top = -1; 
-    int i = 0;
-    while (arr[i] != '\0') 
-    {
-        if (('a' <= arr[i] && arr[i] <= 'z') || ('A' <= arr[i] && arr[i] <= 'Z')) 
-        {
-            struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-            newNode->data = arr[i];
-            newNode->left = NULL;
-            newNode->right = NULL;
-            push(newNode);
-        } 
-        else if (arr[i] == ')')
-        {
-            char temp;
-            while ((temp=pop()) != '(') 
-            {
-                push(temp);
-            }
-        } 
-        else
-        {
-            while (isp(stack[top]) >= icp(arr[i]) && top != -1) 
-            {
-                push1(pop());
-            }
-            push(arr[i]);
+
+// Build expression tree from infix expression
+struct Node* buildTree(char* infix) {
+    int i;
+    for (i = 0; infix[i] != '\0'; i++) {
+        char c = infix[i];
+
+        if (c == ' ') continue;
+
+        if (isalnum(c)) {  // Operand
+            pushNode(newNode(c));
         }
-        i++;
+        else if (c == '(') {
+            pushOp(c);
+        }
+        else if (c == ')') {
+            while (!isEmptyOp() && peekOp() != '(') {
+                char op = popOp();
+                struct Node* right = popNode();
+                struct Node* left = popNode();
+                struct Node* n = newNode(op);
+                n->left = left;
+                n->right = right;
+                pushNode(n);
+            }
+            popOp(); // remove '('
+        }
+        else { // Operator
+            while (!isEmptyOp() && isp(peekOp()) >= icp(c)) {
+                char op = popOp();
+                struct Node* right = popNode();
+                struct Node* left = popNode();
+                struct Node* n = newNode(op);
+                n->left = left;
+                n->right = right;
+                pushNode(n);
+            }
+            pushOp(c);
+        }
     }
-    while (top != -1) 
-    {
-        push1(pop());
+
+    while (!isEmptyOp()) {
+        char op = popOp();
+        struct Node* right = popNode();
+        struct Node* left = popNode();
+        struct Node* n = newNode(op);
+        n->left = left;
+        n->right = right;
+        pushNode(n);
     }
+
+    return popNode(); // Root of expression tree
 }
+
+// Traversals
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%c", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%c", root->data);
+}
+
+int main() {
+    char infix[MAX];
+    printf("Enter an arithmetic expression (infix): ");
+    scanf("%s", infix);
+
+    struct Node* root = buildTree(infix);
+
+    printf("Prefix Expression: ");
+    preorder(root);
+    printf("\n");
+
+    printf("Postfix Expression: ");
+    postorder(root);
+    printf("\n");
+
+    return 0;
 }
